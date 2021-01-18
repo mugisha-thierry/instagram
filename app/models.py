52ff47d92be6beb6815e6f427f950ada736920c9
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-18 11:14:51 +0200

    Created models

--- app/models.py
+++ app/models.py
@@ -3,51 +3,57 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, default='No bio')
-    profile_pic = models.ImageField(upload_to = 'pictures/', null=True)
+    profile = models.ImageField(upload_to='images/', default='a.jpg')
 
     def __str__(self):
         return self.user.username
+    
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-    instance.profile.save()     
+    instance.profile.save()
 
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
-    image_caption = models.CharField(max_length =30)
+    caption = models.CharField(max_length=250, blank=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
-    
 
+    class Meta:
+        ordering = ["-pk"]
 
+    def save_image(self):
+        self.save()
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
 
 
     def delete_image(self):
-        self.delete()    
-
-    def save_image(self):
-        self.save()    
-
-    def get_absolute_url(self):
-        return f"/post/{self.id}"    
-
-    class Meta:
-        ordering = ['date']    
-
-    def total_likes(self):
-        return self.likes.count()    
+        self.delete()
 
     def __str__(self):
-        return self.name    
+        return self.name
+
+    def total_likes(self):
+        return self.likes.count()
+
+class Follow(models.Model):
+    following = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followers = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return self.following
+
 
 class Comment(models.Model):
     comment = models.TextField()
